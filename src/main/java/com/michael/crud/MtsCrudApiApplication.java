84496c0a50bcc7d24dc621d474c3fd0adce82f85
michael Trindade <michaeltrindadedasilva@gmail.com>
2018-07-26 09:36:40 -0400

    Projeto Criado

---
+++ src/main/java/com/michael/crud/MtsCrudApiApplication.java
@@ -0,0 +1,12 @@
+package com.michael.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsCrudApiApplication.class, args);
+	}
+}
